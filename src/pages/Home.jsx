a2c112390f390841356b34fde4131665b136bed7
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 14:00:53 +0300

    fix: responsive and category fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,3 @@
-import HeroCarousel from "../components/HeroCarousel";
-import OurProducts from "../components/OurProducts";
-import ExploreMore from "../components/ExploreMore";
-import Testimonials from "../components/Testimonials";
-import Newsletter from "../components/Newsletter";
-
-
 export default function Home() {
-  return (
-    <>
-      <HeroCarousel />
-      <OurProducts />
-      <ExploreMore />
-      <Testimonials />
-      <Newsletter />
-      
-    </>
-  );
+  return <h1 style={{ marginTop: "100px" }}>HOME WORKS</h1>;
 }
