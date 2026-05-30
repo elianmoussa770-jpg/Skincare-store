e136b19443b04cdf11b7abc0c57bd5c8a4039043
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 14:18:37 +0300

    fix: responsive and category fixes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
+import ShopProvider from "./context/ShopContext";
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <ShopProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ShopProvider>
+  </React.StrictMode>
 );
