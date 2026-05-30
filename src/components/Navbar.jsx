15183c6e3aae2a5d69309bfcfeda194bbda20cc3
Elian Moussa <elianmoussa770@gmail.com>
2026-05-31 00:50:25 +0300

    fix navbar and responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { createPortal } from "react-dom";
-
+import { useNavigate } from "react-router-dom";
 export default function Navbar() {
   const context = useContext(ShopContext);
 
   const cart = context?.cart || [];
   const wishlist = context?.wishlist || [];
 const { search, setSearch } = useContext(ShopContext);
+const [showSearch, setShowSearch] = useState(false);
+const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
@@ -26,13 +28,28 @@ const { search, setSearch } = useContext(ShopContext);
         <Link to="/">Home</Link>
         <Link to="/shop">Shop</Link>
         <Link to="/login">Login</Link>
-<input
-  className="nav-search"
-  type="text"
-  placeholder="Search..."
-  value={search}
-  onChange={(e) => setSearch(e.target.value)}
-/>
+<div className="search-container">
+
+  {/* ICON */}
+  <span onClick={() => setShowSearch(!showSearch)}>
+    🔍
+  </span>
+
+  {/* INPUT */}
+  {showSearch && (
+    <input
+      autoFocus
+      placeholder="Search products..."
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        navigate("/shop"); // 🚀 instantly go to shop
+      }}
+      className="nav-search"
+    />
+  )}
+
+</div>
         <Link to="/wishlist">
           ❤️ {wishlist.length > 0 && <span>•</span>}
         </Link>
@@ -50,6 +67,17 @@ const { search, setSearch } = useContext(ShopContext);
               <Link to="/shop">Shop</Link>
               <Link to="/wishlist">Wishlist</Link>
               <Link to="/cart">Cart</Link>
+              <div className="mobile-search">
+  <input
+    type="text"
+    placeholder="Search products..."
+    value={search}
+    onChange={(e) => {
+      setSearch(e.target.value);
+      navigate("/shop");
+    }}
+  />
+</div>
             </div>
           </div>,
           document.body
