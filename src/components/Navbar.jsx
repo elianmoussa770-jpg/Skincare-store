25378bacf19c86bf50aa2c23bcd4710dde0d58a1
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 16:03:25 +0300

    fix navbar and responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,53 @@
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { createPortal } from "react-dom";
+
 export default function Navbar() {
-  return <h1>NAVBAR TEST</h1>;
+  const context = useContext(ShopContext);
+
+  const cart = context?.cart || [];
+  const wishlist = context?.wishlist || [];
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="navbar">
+
+      <div className="left-nav">
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+
+        <h2>D'you</h2>
+      </div>
+
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/login">Login</Link>
+
+        <Link to="/wishlist">
+          ❤️ {wishlist.length > 0 && <span>•</span>}
+        </Link>
+
+        <Link to="/cart">
+          🛒 {cart.length > 0 && <span>{cart.length}</span>}
+        </Link>
+      </div>
+
+      {menuOpen &&
+        createPortal(
+          <div className="sidebar-overlay" onClick={() => setMenuOpen(false)}>
+            <div className="sidebar" onClick={(e) => e.stopPropagation()}>
+              <Link to="/">Home</Link>
+              <Link to="/shop">Shop</Link>
+              <Link to="/wishlist">Wishlist</Link>
+              <Link to="/cart">Cart</Link>
+            </div>
+          </div>,
+          document.body
+        )}
+    </div>
+  );
 }
