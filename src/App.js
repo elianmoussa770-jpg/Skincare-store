088ebca1a867381e7cb17b37921f7bc493f0af29
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 13:54:36 +0300

    fix: responsive and category fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import FAQ from "./pages/FAQ";
+import Privacy from "./pages/Privacy";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+
+      <Home />
+
+      <Footer />
+    </>
+  );
 }
