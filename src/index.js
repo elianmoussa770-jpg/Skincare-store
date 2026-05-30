2b88d00bda81f4120f988a953ef6a0dd4321bcc8
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 13:36:08 +0300

    fix: responsive and category fixes

--- src/index.js
+++ src/index.js
@@ -3,24 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import ShopProvider from "./context/ShopContext";
-import { BrowserRouter } from "react-router-dom";
-
-root.render(
-  <BrowserRouter basename="/Skincare-store">
-    <App />
-  </BrowserRouter>
-);
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter basename="/Skincare-store">
-    <App />
-  </BrowserRouter>
-);
+
 root.render(
   <ShopProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/Skincare-store">
       <App />
     </BrowserRouter>
   </ShopProvider>
