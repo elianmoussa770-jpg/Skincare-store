d24bc5904eb16bbad5a989ca1067cf6545499de1
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 15:20:11 +0300

    fix navbar and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-
 export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
-  );
+  return <h1>APP IS WORKING</h1>;
 }
