68aa61c5a8e0b786b9c5c63dac14171f031c6756
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 15:30:00 +0300

    fix navbar and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import FAQ from "./pages/FAQ";
+import Privacy from "./pages/Privacy";
+import Category from "./pages/Category";
+
 export default function App() {
-  return <h1>APP IS WORKING</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/faq" element={<FAQ />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/category/:name" element={<Category />} />
+    </Routes>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
+import ShopProvider from "./context/ShopContext";
+import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <ShopProvider>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ShopProvider>
+);
