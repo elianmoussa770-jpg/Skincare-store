dba68b2a8dd77c8147e8e39e2964965b0ca78e8e
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 13:56:35 +0300

    fix: responsive and category fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Login from "./pages/Login";
@@ -7,13 +10,23 @@ import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
 import FAQ from "./pages/FAQ";
 import Privacy from "./pages/Privacy";
+import Category from "./pages/Category";
 
 export default function App() {
   return (
     <>
       <Navbar />
 
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/category/:name" element={<Category />} />
+      </Routes>
 
       <Footer />
     </>
