1b155014fcd7d37c0ed742799bd9af91c5a4869e
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 20:34:45 +0300

    fix navbar and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Login from "./pages/Login";
