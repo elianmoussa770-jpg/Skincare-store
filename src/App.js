9e389b3a8aeb6d0307d00cfcb8ba21cfeaaeff7e
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 15:58:20 +0300

    fix navbar and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import FAQ from "./pages/FAQ";
+import Privacy from "./pages/Privacy";
+import Category from "./pages/Category";
+
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 export default function App() {
   return (
     <>
-      <h1>TEST PAGE</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/category/:name" element={<Category />} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import ShopProvider from "./context/ShopContext";
-import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
