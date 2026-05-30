1d9338d0b579eac8ab458b5062bf2711d099b7e6
Elian Moussa <elianmoussa770@gmail.com>
2026-05-30 14:53:24 +0300

    fix navbar and responsiveness

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ShopProvider from "./context/ShopContext";
 import { HashRouter } from "react-router-dom";
+import ShopProvider from "./context/ShopContext";
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
